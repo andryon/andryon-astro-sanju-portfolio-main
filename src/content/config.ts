a346b235a4540943cc90f053a210d8863cd6d5bc
Andri Putro <andriantocputro@gmail.com>
2025-03-29 23:01:06 +0700

    .

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,7 +19,7 @@ const writing = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		// description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
